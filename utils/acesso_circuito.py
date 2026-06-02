87ca4a4b799a1b9fa321e0c8b8b68d39b0f19d03
Pedro Lima <105872928+Pedro101520@users.noreply.github.com>
2026-06-02 10:38:06 -0300

    Ajustes para o deploy

--- utils/acesso_circuito.py
+++ utils/acesso_circuito.py
@@ -1,7 +1,7 @@
 import json
 
 def circuito():
-    with open("assets\circuitos.json", "r", encoding="utf-8") as arquivo:
+    with open("assets/circuitos.json", "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
     return dados
     
