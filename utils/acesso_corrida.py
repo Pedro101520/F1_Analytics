afac79f525a0290ff8595ee9d560c83cd155cccc
Pedro Lima <105872928+Pedro101520@users.noreply.github.com>
2026-06-02 10:41:32 -0300

    Ajuste deploy

--- utils/acesso_corrida.py
+++ utils/acesso_corrida.py
@@ -1,7 +1,10 @@
 import json
+import os
 
 def corrida():
-    with open("assets/corrida.json", "r", encoding="utf-8") as arquivo:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    caminho = os.path.join(BASE_DIR, "..", "assets", "corrida.json")
+    with open(caminho, "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
     return dados
     
